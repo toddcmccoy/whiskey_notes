0f1d748965f1f6abfa4d8a05dfa6cc44e43efb80
Todd McCoy <tcmccoy@gmail.com>
2022-08-02 15:52:11 -0500

    working on views, specifically aroma rating

---
+++ src/components/AromaRating/AromaRating.jsx
@@ -0,0 +1,55 @@
+import { useState} from 'react'
+import { useHistory } from "react-router-dom"
+import { useDispatch } from 'react-redux'; 
+
+function AromaRating(){
+
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const [aromaRating, setAromaRating] = useState(1);
+
+    const handleGoBack = () => {
+        history.push('/')
+    };
+
+// sending the selected rating to the store and ensuring that the submission is in the range of 1 to 5
+    const handleSubmit = () => {
+        if (aromaRating >= 1) {
+            dispatch ({
+            type: 'ADD_AROMA_RATING',
+            payload: Number(aromaRating)
+        })
+        history.push('/aroma_notes')
+        } else {
+            alert ('You must enter a number between 1 and 10. There are no wrong answers.')
+        };
+        
+    }
+    return(
+        <div className = "container">
+        <div className = "question-card">
+            <h1>How would you rate the aroma intensity of this whiskey?</h1>
+            <h2>Please select a number between 1 and 10.</h2>
+            <h3>1 being 'It doesn't smell like much of anything. No alcohol burn and no detectable scents.'</h3>
+            <h3>10 being 'Wow, those scents are powerful. And/or I think the alcohol vapors permanently damaged my nose.'</h3>
+            <form>
+                <input
+                className= "number-input"
+                type="number"
+                min="1"
+                max="10"
+                value = {aromaRating}
+                onChange={(event) => setAromaRating(event.target.value)}
+                />
+            </form><br />
+            <button onClick={handleGoBack}>Previous</button>
+            <button onClick={handleSubmit}>Next</button>
+        </div>
+    </div>
+    )
+
+
+
+}
+
+export default AromaRating;
