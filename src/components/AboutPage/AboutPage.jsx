b6f10a711f475e1f6e269de4f20c05cd24d44373
Todd McCoy <tcmccoy@gmail.com>
2022-08-12 08:10:47 -0500

    about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import CSP from '../../images/CSP.png'
+import STSlogo from '../../images/STSlogo.png'
+
 
 
 // This is one of our simplest components
@@ -13,15 +16,19 @@ function AboutPage() {
         <div className="aboutpage">
         <img src = "https://media-exp1.licdn.com/dms/image/C4E03AQE7gMoF-dQ1AQ/profile-displayphoto-shrink_800_800/0/1660229802166?e=1665619200&v=beta&t=aLQxfNFBSKcIMXP7-S3x8V6pbFTDjmhb622bj1Sz6j8" height={300} width={300}/>
         <h2>Todd C McCoy</h2>
+        <h4>Executive Bourbon Steward</h4>
+        <h4>Certified Scotch Professional</h4>
+        
+        <a href="https://www.linkedin.com/in/toddcmccoy/">linkedin.com/in/toddcmccoy/</a>
         
             <h4>I would like to thank</h4>
             <p>
             Everyone here at Prime, especially Liz and Dane<br />
             Jemisin cohort<br />
             My partner for their support<br />
-            My parents and family<br />
-            Stave and Thief Society<br />
-            Concil of Whisky Masters<br />
+            My parents and family<br /><br />
+            <img src={STSlogo} width="100px" /><br />  Stave and Thief Society<br /><br />
+            <img src={CSP} width="100px"/>  <br />Council of Whisky Masters<br />
             </p>
         </div>
       </div>
