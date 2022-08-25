be7b515176a6b1c48eb8027a0df8e988edd9fe5a
Todd McCoy <tcmccoy@gmail.com>
2022-08-25 15:24:06 -0500

    removed MUI temporarily

--- src/components/WhiskeyEducation/WhiskeyEducation.jsx
+++ src/components/WhiskeyEducation/WhiskeyEducation.jsx
@@ -1,197 +1,46 @@
-import * as React from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-// import { Box } from '@mui/system';
+import { useHistory } from "react-router-dom"
 
 
-export default function WhiskeyEducation() {
-  return (
-    <div className="question-card"
-    sx={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(2)',
-        gap: 2,
-      }}
-    >
-      <Accordion
-      sx={{
-        width: 500,
-        margin: 2,
-        gridRow: 1,
-        gridColumn: 1 / 2,
-      }}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Scotch</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            The legal stuff - Scotch must be distilled, aged, and botled in Scotland.
+function WhiskeyEducation(){
 
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-      sx={{
-        width: 500,
-        margin: 2,
-      }}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Irish Whiskey</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            The legal stuff - Irish whiskey must be distilled and aged in Ireland.
-            Unique note - Ireland is home to Single Pot Still whiskey, a whiskey produced in a pot still using both malted barley
-            and unmalted barley. The practice began when the British Crown increased taxes on malted barley. Irish distillers reduced the amount
-            of malted barley and added in unmalted barley, thereby screwing over the English.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-      sx={{
-        width: 500,
-        margin: 2,
-        gridRow: 1,
-        gridColumn: 1 / 2,
-      }}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Bourbon</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-          Bourbon is a distinct product of the United States.
-          It must be distilled at no higher than 165 proof from a recipe of at least 51% corn,
-          placed in new charred oak containers at no higher than 125 proof,
-          then finally bottled at no less than 80 proof.
+    const history = useHistory();
 
-          Fun fact: While Kentucky produces 90 to 95 percent of bourbon in the world
-          it can be made anywhere in the United States, including Guam and Puerto Rico.
+    const handleGoDashboard = () => {
+        history.push('/dashboard')
+    };
 
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-      sx={{
-        width: 500,
-        margin: 2,
-      }}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Rye</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Rye whiskey must be produced from at least 51% rye grain.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-      sx={{
-        width: 500,
-        margin: 2,
-      }}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Canadian Whiskey</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            The legal stuff 
+// sending the selected rating to the store and ensuring that the submission is in the range of 1 to 5
+    
+    return(
+        <div className = "container">
+            <div className = "question-card">
+                <h1>What is bourbon?</h1>
 
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-      sx={{
-        width: 500,
-        margin: 2,
-      }}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Japanese Whiskey</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            The legal stuff
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-      sx={{
-        width: 500,
-        margin: 2,
-      }}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>How can I learn more?</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            I highly recommend 
-            <a href="https://www.amazon.com/Tasting-Whiskey-Insiders-Pleasures-Spirits/dp/1612123015"> Tasting Whiskey by Lew Bryson</a>.
-            This book is by far the best and most approachable introduction to the world of whiskey I have found.
+                        <h2>Bourbon as we know it</h2>
+                        <h3>Must be produced in the United States (since 1964)<br/>
+                        Must be aged in a new, charred oak container (since 1938)<br/>
+                        Aged for two years may be labeled “straight”<br/>
+                        Any bourbon aged less than four years must have an age statement on the label<br/>
+                        Must be distilled to less than 160 proof<br/>
+                        Must go into the oak container at 125 proof or less<br/>
+                        Must be bottled at a minimum of 80 proof<br/></h3>
 
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-      sx={{
-        width: 500,
-        margin: 2,
-      }}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Is Jack Daniel's actually Bourbon?</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Technically, yes it is. Old Number 7 meets all the legal requirements for bourbon. 
-            In fact, it is classified as a bourbon by the United States Tax and Trade Bureau.
-            Why don't they call it bourbon? Well, because they don't want to.
-            Pro tip: when visiting Tennessee DO NOT refer to Jack Daniel's as bourbon. 
-            It will not go well for you. 
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
+                        <h2>Bottle-in-Bond Act of 1897</h2>
+                        <h3>Must be distilled by one distiller<br/>
+                        Distilled during one distilling season (January to June or July to December)<br/>
+                        Aged in new charred oak containers for a minimum of four years in a Federally bonded warehouse<br/>
+                        Bottled at 100 proof in glass bottles</h3>
+                    <br />
+                    <p>Fun fact: While Kentucky produces 90 to 95 percent of bourbon in the world
+                      it can be made anywhere in the United States, including Guam and Puerto Rico.</p>
+                <br />
+            <button onClick={handleGoDashboard}>Back to dashboard</button>
+        </div>
     </div>
-  );
+    )
+
+
+
 }
 
+export default WhiskeyEducation;
